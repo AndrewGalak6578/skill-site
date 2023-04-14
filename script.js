57b64b3a9336ce1ac910563dfd28c6f9a557ba19
AndrewGalak6578 <andrew.galaktionov@inbox.ru>
2023-04-14 20:15:37 +0600

    little upgrades

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
+const Web3 = require('web3');
+
+// Создание экземпляра Web3
+const web3 = new Web3(window.ethereum);
 const menuItems = document.querySelectorAll('.menu a');
+const connectMetamaskButton = document.querySelector('.connect-metamask');
 
 menuItems.forEach(item => {
   item.addEventListener('mouseenter', () => {
@@ -9,15 +14,26 @@ menuItems.forEach(item => {
     item.classList.remove('animated', 'pulse');
   });
 });
-async function connect() {
+async function connectMetamask() {
   try {
-    // запрашиваем подключение к кошельку MetaMask
-    await window.ethereum.enable();
-    // Если пользователь подключился, выводим его адрес кошелька
-    const accounts = await window.ethereum.request({ method: 'eth_accounts' });
-    const address = accounts[0];
-    alert(`Connected to MetaMask with address: ${address}`);
+    // Проверяем, есть ли Metamask
+    if (typeof window.ethereum === 'undefined') {
+      throw new Error('Metamask is not installed.');
+    }
+
+    // Подключаем Metamask
+    await window.ethereum.request({ method: 'eth_requestAccounts' });
+
+    // Место для вашего кода, который будет выполняться после успешного подключения к Metamask
   } catch (error) {
     console.error(error);
+    alert('Failed to connect to Metamask. Please make sure it is installed and try again later.');
   }
+}
+
+if (typeof window.ethereum !== 'undefined') {
+  connectMetamaskButton.style.display = 'block';
+  connectMetamaskButton.addEventListener('click', connectMetamask);
+} else {
+  connectMetamaskButton.style.display = 'none';
 }
